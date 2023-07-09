fa18f38be3ac86c741c18f089209144b305b6c41
ivansiuzzzzz <ivan.siu@icloud.com>
2023-07-09 22:27:45 +0800

    add meal detail

--- lib/screen/meal_detail.dart
+++ lib/screen/meal_detail.dart
@@ -9,13 +9,48 @@ class MealDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(meal.title)),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    );
+        appBar: AppBar(title: Text(meal.title)),
+        body: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            ...meal.ingredients.map((e) => Text(
+                  e,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                )),
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            ...meal.steps.map((e) => Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                  child: Text(
+                    e,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                )),
+          ],
+        ));
   }
 }
